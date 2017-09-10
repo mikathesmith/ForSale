a95289eda2d2c4962eaaad7b26dc860d4378827d
kimmy241 <kimlouw.kl@gmail.com>
2017-09-10 14:33:59 +1200

    Phase 2 completed

--- Test.java
+++ Test.java
@@ -7,6 +7,8 @@ package forsale;
 
 import java.util.ArrayList;
 
+import javax.smartcardio.Card;
+
 import java.util.*;
 import java.lang.*;
 
@@ -26,6 +28,7 @@ public class Test {
     private static final int LOW_RANGE =16;
     private static final Random random = new Random();
     private static final int CHEQUE_RANGE = 6; 
+    private static int index30,index29,index28,index27,index2, index1;
     
     
     /**
@@ -199,10 +202,28 @@ public class Test {
         			ArrayList<Card> myCurrentHand = p.getCards(); //the houses that I've bought in phase 1
         			ArrayList<Integer> cards= new ArrayList<Integer>();  
         			ArrayList<Integer> chequesLeft= s.getChequesRemaining();  //the cheques that are still left in the deck
-        		
-        			
-        	
-        			
+
+                    for (int i = 0 ; i < myCurrentHands().size()-1 ; i++){
+                        if(myCurrentHand(i).getQuality().indexOf(30) != -1){
+                            index30=i;
+                        }
+                        if(myCurrentHand(i).getQuality().indexOf(29) != -1){
+                            index29=i;
+                        }
+                        if(myCurrentHand(i).getQuality().indexOf(28) != -1){
+                            index28=i;
+                        }
+                        if(myCurrentHand(i).getQuality().indexOf(27) != -1){
+                            index27=i;
+                        }
+                        if(myCurrentHand(i).getQuality().indexOf(2) != -1){
+                            index2=i;
+                        }
+                        if(myCurrentHand(i).getQuality().indexOf(1) != -1){
+                            index1=i;
+                        }
+                    }
+
         			for(Card c : myCurrentHand){ //fill hand with card integer values 
         				cards.add(c.getQuality());
         			}
@@ -218,19 +239,19 @@ public class Test {
                     if(cards.indexOf(30) != -1 && cheques.contains(15)){
                     	if (cards.indexOf(29) != -1) { //also have a 29
                     		 if(chequesLeft.contains(15)) { // this is the first 15
-                                 return 29; //play 29 on first 15 cheque - cards.get(29);
+                                 return myCurrentHand.get(index29); //play 29 on first 15 cheque - cards.get(29);
                                  //return myCurrentHand.get(Card.getQuality(29);
                              } else{
-                                 return 30; //play 30 on second 15 cheque 
+                                 return myCurrentHand.get(index30); //play 30 on second 15 cheque 
                              }
                     	}else{ //dont have a 29
                     		 if (ran == 0) {
                                  if (chequesLeft.contains(15)) { // play 30 on first 15
-                                 	return 30;
+                                 	return myCurrentHand.get(index30);
                                  }
                              } else{ // r == 1
                             	 if (!chequesLeft.contains(15)) { // play 30 on second 15
-                                  	return 30;
+                                  	return myCurrentHand.get(index30);
                                   }
                             	 
                              }
@@ -240,45 +261,55 @@ public class Test {
                     //check if we have a 29
                     if (cards.indexOf(29) != -1) {
                             if (cheques.contains(15) && !chequesLeft.contains(15)) { // second 15
-                                return myCurrentHand.get(myCurrentHand.getQuality().indexOf(29)); //wait for the second 15 cheque
+                                return myCurrentHand.get(index29); //wait for the second 15 cheque
                             }
                     }
 
                     //check if we have a 28
                     if (cards.indexOf(28) != -1) {
                         if (cheques.contains(14)) { // first 14
-                            return 28; // play 28 on first 14
+                            return myCurrentHand.get(index28); // play 28 on first 14
                         }
                     }
 
+                    //check if we have a 27
+                    if (cards.indexOf(27) != -1) {
+                        if (cheques.contains(14) && !chequesLeft.contains(14)) { // second 14
+                            return myCurrentHand.get(index27); // play 27 on second 14
+                        }
+                    }
+
+                    // check if we have a 2 and not a 1
                     if (cards.indexOf(2) != -1 && cards.indexOf(1) == -1) {
-                        
-                        if (cheques.get(0) >= 6) {
-                            return 2;
+                        if (cheques.get(0) >= 6) { // lowest cheque value is 6
+                            return myCurrentHand.get(index2);
                         }
                     }
 
+                    // check if we have a 1 and not a 2
                     if (cards.indexOf(1) != -1 && cards.indexOf(2) == -1) {
-                        if (cheques.get(0) >= 6) {
-                            return 1;
+                        if (cheques.get(0) >= 6) { // lowest cheque value is 6
+                            return myCurrentHand.get(index1);
                         }
                     }
 
                     if(cheques.contains(0)){
-                        return cards.get(cards.size()/2); //return middle card
+                        return myCurrentHand.get((myCurrentHand.size()/2)+1); //return middle card
                     }
 
 
                     //Generic method 
                     int chequeRange = cheques.get(cheques.size()-1) - cheques.get(0); //range between highest and lowest cheque
                     if(chequeRange < CHEQUE_RANGE){ //low range
-
+                        return myCurrentHand.get(0); // play lowest card
                     }else{ //high range
-
+                        if (cheques.get(5)  == 13) { // check if highest cheque is a 13
+                            return myCurrentHand.get(myCurrentHand.size()-1); // play highest card
+                        } else {
+                            return myCurrentHand.get((myCurrentHand.size()/2)+1); //return middle card
+                        }
                     }
 
- 
-        		
                // System.out.println(p.getCards());
                 return p.getCards().get((int) (Math.random()*p.getCards().size()));
             }
